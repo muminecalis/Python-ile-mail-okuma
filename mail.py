e2608984326634d62f0f1b588bf81fb5c3b809c3
muminecalis <44002438+muminecalis@users.noreply.github.com>
2020-09-29 18:49:13 +0300

    Add files via upload
    Python üzerinden mail okumak üzerine yazılmış kod çalışmasıdır.

---
+++ mail.py
@@ -0,0 +1,22 @@
+import poplib
+
+org_email="@gmail.com"
+
+from_email_user="//e posta adresinizi giriniz"
+
+
+from_email_pwd="//şifrenizi giriniz"
+
+SMTP_SERVER = "pop.gmail.com"
+SMTP_PORT   = 993
+
+def readmail():
+
+
+
+
+
+    
+
+
+    
